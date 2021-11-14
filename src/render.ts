026ab7a66d451e97c129ee8f88dfc8635b0d9f6b
Ryan Geyer <ryan@waymark.com>
2021-11-14 00:48:35 -0500

    Clean up handling for window resizing

--- src/render.ts
+++ src/render.ts
@@ -14,43 +14,54 @@ const textures = [
   }),
 ];
 
+let textureResizeAnimationFrameId;
+
+let displayTextureIndex = 0;
+
+// Returns the texture that isn't currently set on the display sprite
+// and should therefore be rendered to
+function getRenderTargetTexture() {
+  return textures[1 - displayTextureIndex];
+}
+
+function getDisplayTexture() {
+  return textures[displayTextureIndex];
+}
+
+function swapDisplayTexture() {
+  displayTextureIndex = 1 - displayTextureIndex;
+  displaySprite.texture = textures[displayTextureIndex];
+}
+
 window.addEventListener(
   "resize",
   () => {
-    // Resize the textures when the window resizes
-    const newWidth = window.innerWidth;
-    const newHeight = window.innerHeight;
+    cancelAnimationFrame(textureResizeAnimationFrameId);
 
-    textures[0].resize(newWidth, newHeight);
-    textures[1].resize(newWidth, newHeight);
+    textureResizeAnimationFrameId = requestAnimationFrame(() => {
+      // Resize the textures when the window resizes
+      const newWidth = window.innerWidth;
+      const newHeight = window.innerHeight;
+      getRenderTargetTexture().resize(newWidth, newHeight);
+      render();
+      getDisplayTexture().resize(newWidth, newHeight);
+    });
   },
   { passive: true }
 );
 
-let currentTextureIndex = 0;
-
-const getCurrentTexture = () => textures[currentTextureIndex];
-
-function swapCurrentTexture() {
-  currentTextureIndex = 1 - currentTextureIndex;
-
-  return getCurrentTexture();
-}
-
-const displaySprite = new PIXI.Sprite(getCurrentTexture());
+const displaySprite = new PIXI.Sprite(textures[displayTextureIndex]);
 displaySprite.zIndex = 1;
 displaySprite.filters = [paintShader];
 
 pixiApp.stage.addChild(displaySprite);
 
 export default function render() {
-  const currentTexture = swapCurrentTexture();
-
   pixiApp.renderer.render(pixiApp.stage, {
-    renderTexture: currentTexture,
+    renderTexture: getRenderTargetTexture(),
   });
 
-  displaySprite.texture = currentTexture;
+  swapDisplayTexture();
 
   // Update the random seed for our shaders for next frame
   paintShader.uniforms.randSeed = Math.random();
